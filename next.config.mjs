f76d5ca0801eccf323bccdc98a77262c0c0af914
Willem Verbuyst <willemverbuyst@gmail.com>
2024-08-09 07:01:42 +0200

    Add gh-actions to publish to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/framer-motion-playground",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
