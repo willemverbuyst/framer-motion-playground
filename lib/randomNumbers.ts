66a6b49c04ebc13e788e504bb9c50e93db351a49
Willem Verbuyst <willemverbuyst@gmail.com>
2024-08-11 09:15:56 +0200

    Generate random numbers

---
+++ lib/randomNumbers.ts
@@ -0,0 +1,14 @@
+import * as _ from "lodash";
+
+export function generateRandomNumbers(
+  lower: number,
+  upper: number,
+  numberOfRandomNumbers: number,
+) {
+  return _.chain(_.range(numberOfRandomNumbers))
+    .map(() => _.random(lower, upper))
+    .sortBy()
+    .map((n) => _.padStart(_.toString(n), 6, "0"))
+    .chunk(10)
+    .value();
+}
