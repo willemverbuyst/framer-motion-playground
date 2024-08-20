dd7f500f4a0ec064ba9e283c9c83e9ab908f76a6
Willem Verbuyst <willemverbuyst@gmail.com>
2024-08-20 19:54:00 +0200

    fix: format with biome

--- lib/randomNumbers.ts
+++ lib/randomNumbers.ts
@@ -1,16 +1,16 @@
-import { toString as _toString, chain, padStart, random, range, } from "lodash";
+import { toString as _toString, chain, padStart, random, range } from "lodash";
 
 export function generateRandomNumbers(
 	lower: number,
 	upper: number,
 	numberOfRandomNumbers: number,
 ) {
-  const arr = range(numberOfRandomNumbers);
+	const arr = range(numberOfRandomNumbers);
 
-  return chain(arr)
-    .map(() => random(lower, upper))
-    .sortBy()
-    .map((n) => padStart(_toString(n), 6, "0"))
-    .chunk(10)
-    .value();
+	return chain(arr)
+		.map(() => random(lower, upper))
+		.sortBy()
+		.map((n) => padStart(_toString(n), 6, "0"))
+		.chunk(10)
+		.value();
 }
