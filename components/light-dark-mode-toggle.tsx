cbd171e0156153a12775b0c9a96978ec28b1d674
Willem Verbuyst <willemverbuyst@gmail.com>
2024-09-03 04:25:32 +0200

    feat: create dark mode toggle component

---
+++ components/light-dark-mode-toggle.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+export default function LightDarkModeToggle() {
+	const [isDarkMode, setIsDarkMode] = useState(true);
+
+	return (
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger
+					onClick={() => {
+						setIsDarkMode((prev) => !prev);
+						document.body.classList.toggle("dark");
+					}}
+				>
+					{isDarkMode ? <MoonIcon /> : <SunIcon />}
+				</TooltipTrigger>
+				<TooltipContent>
+					{isDarkMode ? "Switch to light mode" : "Switch to dark mode"}
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	);
+}
