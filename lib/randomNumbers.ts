d80aed3d3040abd86053f29e81487fe4b4267b61
Willem Verbuyst <willemverbuyst@gmail.com>
2024-09-21 04:37:50 +0200

    fix: focus lodash imports

--- lib/randomNumbers.ts
+++ lib/randomNumbers.ts
@@ -1,16 +1,22 @@
-import _ from "lodash";
+import {
+	chain as _chain,
+	padStart as _padStart,
+	random as _random,
+	range as _range,
+	toString as _toString,
+} from "lodash";
 
 export function generateRandomNumbers(
 	lower: number,
 	upper: number,
 	numberOfRandomNumbers: number,
 ) {
-	const arr = _.range(numberOfRandomNumbers);
+	const arr = _range(numberOfRandomNumbers);
 
-	return _(arr)
-		.map(() => _.random(lower, upper))
+	return _chain(arr)
+		.map(() => _random(lower, upper))
 		.sortBy()
-		.map((n) => _.padStart(_.toString(n), 6, "0"))
+		.map((n) => _padStart(_toString(n), 6, "0"))
 		.chunk(10)
 		.value();
 }
